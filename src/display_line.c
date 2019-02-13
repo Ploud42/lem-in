0ed06c7a8c66dd31a8d3d5158acb92740a48bcae
Julien Sobel <juliensobel@MacBook-Pro-de-Julien.local>
2019-02-13 16:15:01 +0100

    small additions

--- src/display_line.c
+++ src/display_line.c
@@ -6,7 +6,7 @@
 /*   By: jsobel <jsobel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 17:05:57 by jsobel            #+#    #+#             */
-/*   Updated: 2019/02/11 18:57:04 by jsobel           ###   ########.fr       */
+/*   Updated: 2019/02/12 19:22:17 by juliensobel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,12 @@ void	ft_display_lines(t_lemin *data)
 {
 	while (data->ants)
 	{
-		data->i = 0;
-		
+		data->i = 1;
+		data->l = data->way;
+		while (data->l)
+		{
+			if (data->ants)
+				
+		}
 	}
 }
